a3480c7e00634a41a898faaa157d058c17aacfb6
karthikesh75 <karthikeshwaran99@gmail.com>
2025-01-08 20:31:33 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import Home from './Home';
+import AppRouter from './Router';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
